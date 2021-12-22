d8b4b77017a75e5f225c99a95180cd5616833e1b
zuigzm <zuigzm@gmail.com>
2021-12-22 17:59:24 +0800

    feat: zefyr editor

--- lib/src/editor_page.dart
+++ lib/src/editor_page.dart
@@ -20,62 +20,116 @@ class EditorPageState extends State<EditorPage> {
   late ZefyrController _controller;
 
   /// Zefyr editor like any other input field requires a focus node.
-  late FocusNode _focusNode;
+  late final FocusNode _focusNode = FocusNode();
+
+  // void _handleSettingsLoaded(Settings value) {
+  //   setState(() {
+  //     _settings = value;
+  //     _loadFromAssets();
+  //   });
+  // }
 
   @override
   void initState() {
     super.initState();
-    _focusNode = FocusNode();
-    _loadDocument().then((document) {
-      setState(() {
-        _controller = ZefyrController(document);
-      });
-    });
+
+    final document = _loadDocument();
+    _controller = ZefyrController(document);
+    // _loadDocument().then((document) {
+    //   setState(() {
+    //     _controller = ZefyrController(document);
+    //   });
+    // });
   }
 
   @override
   Widget build(BuildContext context) {
-    final body = (_controller == null)
-        ? const Center(child: CircularProgressIndicator())
-        : ZefyrField(
-            padding: const EdgeInsets.all(16),
-            controller: _controller,
-            focusNode: _focusNode,
-          );
+    // 自定义 toolbar
+    final toolbar = ZefyrToolbar(children: [
+      ToggleStyleButton(
+        attribute: NotusAttribute.bold,
+        icon: Icons.format_bold,
+        controller: _controller,
+      ),
+      const SizedBox(width: 1),
+      ToggleStyleButton(
+        attribute: NotusAttribute.italic,
+        icon: Icons.format_italic,
+        controller: _controller,
+      ),
+      VerticalDivider(indent: 16, endIndent: 16, color: Colors.grey.shade400),
+      SelectHeadingStyleButton(controller: _controller),
+      VerticalDivider(indent: 16, endIndent: 16, color: Colors.grey.shade400),
+      ToggleStyleButton(
+        attribute: NotusAttribute.block.numberList,
+        controller: _controller,
+        icon: Icons.format_list_numbered,
+      ),
+      ToggleStyleButton(
+        attribute: NotusAttribute.block.bulletList,
+        controller: _controller,
+        icon: Icons.format_list_bulleted,
+      ),
+      VerticalDivider(indent: 16, endIndent: 16, color: Colors.grey.shade400),
+      ToggleStyleButton(
+        attribute: NotusAttribute.block.quote,
+        controller: _controller,
+        icon: Icons.format_quote,
+      ),
+    ]);
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text(S.of(context).title),
-        leading: Builder(
-            builder: (BuildContext context) => IconButton(
-                  icon: const Icon(Icons.menu),
-                  tooltip: "Navigration",
-                  onPressed: () => Scaffold.of(context).openDrawer(),
-                )),
-        actions: <Widget>[
-          Builder(
-            builder: (context) => IconButton(
-              icon: const Icon(Icons.save),
-              onPressed: () => _saveDocument(context),
+        appBar: AppBar(
+          title: Text(S.of(context).title),
+          leading: Builder(
+              builder: (BuildContext context) => IconButton(
+                    icon: const Icon(Icons.menu),
+                    tooltip: "Navigration",
+                    onPressed: () => Scaffold.of(context).openDrawer(),
+                  )),
+          actions: <Widget>[
+            Builder(
+              builder: (context) => IconButton(
+                icon: const Icon(Icons.save),
+                onPressed: () => _saveDocument(context),
+              ),
+            )
+          ],
+        ),
+        drawer: const DrawerWidget(),
+        body: Column(
+          children: [
+            Expanded(
+              child: Container(
+                color: Colors.white,
+                padding: const EdgeInsets.all(16.0),
+                child: ZefyrEditor(
+                  controller: _controller,
+                  focusNode: _focusNode,
+                  autofocus: true,
+                ),
+              ),
             ),
-          )
-        ],
-      ),
-      drawer: const DrawerWidget(),
-      body: body,
-    );
+            Divider(height: 1, thickness: 1, color: Colors.grey.shade200),
+            ZefyrToolbar.basic(
+              controller: _controller,
+            ),
+          ],
+        ));
   }
 
   /// Loads the document to be edited in Zefyr.
-  Future<NotusDocument> _loadDocument() async {
-    final file = File(Directory.systemTemp.path + '/quick_start.json');
-    if (await file.exists()) {
-      final contents = await file.readAsString().then(
-          (data) => Future.delayed(const Duration(seconds: 1), () => data));
-      return NotusDocument.fromJson(jsonDecode(contents));
-    }
-    final delta = Delta()..insert('Zefyr Quick Start\n');
-    return NotusDocument()..compose(delta, ChangeSource.local);
+  NotusDocument _loadDocument() {
+    // final file = File(Directory.systemTemp.path + '/quick_start.json');
+    // if (await file.exists()) {
+    //   final contents = await file.readAsString().then(
+    //       (data) => Future.delayed(const Duration(seconds: 1), () => data));
+    //   return NotusDocument.fromJson(jsonDecode(contents));
+    // }
+    // final delta = Delta()..insert('Zefyr Quick Start\n');
+    // return NotusDocument()..compose(delta, ChangeSource.local);
+    final Delta delta = Delta()..insert("Zefyr Quick Start\n");
+    return NotusDocument.fromDelta(delta);
   }
 
   void _saveDocument(BuildContext context) {
